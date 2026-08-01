d396a02cc6918521f858d4c29abcb39750a8f484
faturrahman04 <faturrahmanalfarisi449@gmail.com>
2026-08-01 18:11:42 +0700

    Login and register feature

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,16 +1,21 @@
-interface Payload {
-    firstName: string,
-    lastName: string | null | undefined,
-    username: string,
-    email: string,
+interface RegisterPayload {
+    firstName: string
+    lastName: string | null | undefined
+    username: string
+    email: string
+    password: string
+}
+
+interface LoginPayload {
+    email: string
     password: string
 }
 
 export const userRegister = async ({
     firstName, lastName, username, email, password
-}: Payload
+}: RegisterPayload
 ) => {
-    const endpoint = "http://localhost:3000/api/auth/register"
+    const endpoint: string = "http://localhost:3000/api/auth/register"
     const payload = {
         firstName, lastName, username, email, password
     }
@@ -28,5 +33,28 @@ export const userRegister = async ({
         console.log(result)
     } catch (err) {
         console.error("Error during registeres", err)
+    }
+}
+
+export const userLogin = async ({
+    email, password
+}: LoginPayload) => {
+    const endpoint = "http://localhost:3000/api/auth/login"
+    const payload = {
+        email, password
+    }
+    
+    try {
+        const response = await fetch(endpoint, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        const result = await response.json()
+        console.log(result)
+    } catch (err) {
+        console.error("Login failed", err)
     }
 }
--- src/features/auth/Login.tsx
+++ src/features/auth/Login.tsx
@@ -1,16 +1,37 @@
+import { useState } from "react"
 import { Button } from "../../components/Button"
 import { Input } from "../../components/Input"
 import { FormLayout } from "./FormLayout"
+import { userLogin } from "../../api/auth"
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData(prev => ({
+            ...prev,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        await userLogin(formData)
+    }
+
     return (
-        <FormLayout>
+        <FormLayout handleSubmitForm={handleLogin}>
             <Input 
                 placeholder="user@gmail.com" 
                 field="email" 
                 type="email" 
                 required={true} 
                 className={'bg-gray-200/80 px-4 py-3 rounded-md w-full mb-4'} 
+                onChange={handleChange}
+                value={formData.email}
             />
 
             <Input 
@@ -19,6 +40,8 @@ const Login = () => {
                 type="password" 
                 required={true} 
                 className={'bg-gray-200/80 px-4 py-3 rounded-md w-full mb-6'} 
+                onChange={handleChange}
+                value={formData.password}
             />
 
             <Button 
